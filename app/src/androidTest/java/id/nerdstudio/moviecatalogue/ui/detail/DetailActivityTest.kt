f93921e2cfcb50d068054e2f136fa934cb8a9807
fadeltd <fadeltd@hotmail.com>
2019-07-06 18:10:53 +0700

    fix review, clean import resources

--- app/src/androidTest/java/id/nerdstudio/moviecatalogue/ui/detail/DetailActivityTest.kt
+++ app/src/androidTest/java/id/nerdstudio/moviecatalogue/ui/detail/DetailActivityTest.kt
@@ -9,8 +9,10 @@ import org.junit.Test
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import id.nerdstudio.moviecatalogue.R
 import id.nerdstudio.moviecatalogue.data.Type
 
